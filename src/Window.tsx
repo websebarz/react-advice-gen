513fff6123a81bd502552c5e0e0c20083beedd10
Seb Arz <109311254+websebarz@users.noreply.github.com>
2022-09-14 17:42:17 -0500

    API request completed

--- src/Window.tsx
+++ src/Window.tsx
@@ -1,18 +1,47 @@
+import { useEffect, useState } from "react";
+
 function Window() {
+  interface Advice {
+    slip: {
+      id: number;
+      advice: string;
+    };
+  }
+
+  const [adviceEl, setAdviceEl] = useState<Advice>({
+    slip: {
+      id: 0,
+      advice: "",
+    },
+  });
+  const [clicked, setClicked] = useState(false);
+
+  const toggleEl = () => {
+    setClicked((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    const getData = () => {
+      fetch("https://api.adviceslip.com/advice")
+        .then((res) => res.json())
+        .then((data) => setAdviceEl(data));
+    };
+
+    getData();
+  }, [clicked]);
+  console.log(adviceEl);
+
   return (
     <div className="container--el">
       <h2 className="advice-title--el">ADVICE #117</h2>
-      <p className="advice--el">
-        “It is easy to sit up and take notice, what's difficult is getting up
-        and taking action.”
-      </p>
+      <p className="advice--el">{adviceEl.slip.advice}</p>
       <div className="underline--el">
         <hr className="line--el" />
         <span className="under--el"></span>
         <span className="under--el"></span>
         <hr className="line--el" />
       </div>
-      <span className="dice--el">
+      <span className="dice--el" onClick={toggleEl}>
         <span>
           <i>
             <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
