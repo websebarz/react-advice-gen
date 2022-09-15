34eec76bd9936ade5666d9e9608b690208cbe48f
Seb Arz <109311254+websebarz@users.noreply.github.com>
2022-09-15 11:59:51 -0500

    Fixed underline position

--- src/Window.tsx
+++ src/Window.tsx
@@ -29,7 +29,7 @@ function Window() {
 
     getData();
   }, [clicked]);
-  console.log(adviceEl);
+  // console.log(adviceEl);
 
   return (
     <div className="container--el">
@@ -43,7 +43,7 @@ function Window() {
       </div>
       <span className="dice--el" onClick={toggleEl}>
         <span>
-          <i>
+          <i className="drop--el">
             <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
